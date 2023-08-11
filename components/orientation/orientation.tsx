02e6a1d180ebf102070d5a1acad9067b325278b0
tuvshinbaatar <71537658+tuvshinbaatar@users.noreply.github.com>
2023-08-11 19:51:23 +1000

    Display second commit 11/8/23

--- components/orientation/orientation.tsx
+++ components/orientation/orientation.tsx
@@ -12,15 +12,85 @@ import {
 } from 'react-native';
 
 
+
+//Display orientation input
+enum orientation {
+	none = '',
+	normal = 'normal',
+	right = 'right',
+	inverted = 'inverted',
+	left = 'left'
+}
+
+//Display payload
+let player_id: number = -1;
+let client_id: number = -1;
+let session_id: number = -1;
+
+let API: [number, number, number]
+API = [player_id, client_id, session_id]
+
+var upPress = false;
+
+function pressNormal(): boolean {
+  upPress = true;
+  Alert.alert("true");
+  return upPress;
+}
+function pressInverse(): boolean {
+  upPress = true;
+  Alert.alert("true");
+  return upPress;
+}
+function pressLeft(): boolean {
+  upPress = true;
+  Alert.alert("true");
+  return upPress;
+}
+function pressRight(): boolean {
+  upPress = true;
+  Alert.alert("true");
+  return upPress;
+}
+
 export default class Orientation extends React.Component { 
     render() { 
       return (
-        <View> 
+        <View style={styles.viewStyle}>
+          <View>
             <Button
-                title="Press me"
-                onPress={() => Alert.alert('Simple Button pressed')}
-            />
-        </View> 
+              title="Normal"
+              onPress={() => console.log(pressNormal)} />
+          </View>
+          
+          <View style={{ flexDirection:"row"}}>
+              <Button
+                title="Left"
+                onPress={() => console.log(pressLeft)} />
+              <Button
+                title="Right"
+                onPress={() => console.log(pressRight)} />
+            </View>
+            
+            <View>
+              <Button
+                title="Inverse"
+                onPress={() => console.log(pressInverse)} />
+            </View>
+
+          </View>
       );
     }
   }
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: '#31C283',
+    justifyContent: 'center',
+  },
+  viewStyle:{
+    alignItems: 'center',
+    fontSize: 30,
+    color: "green"
+  }
+})
