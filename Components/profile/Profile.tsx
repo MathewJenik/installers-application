277dde4bb0e827c78d2b01427bd6b4b17abb756f
GidiKho <gidion.kho@gmail.com>
2023-09-20 20:03:09 +1000

    Changes: - Using ViewContainer to display the card alike with the margins and paddings - Adding meanings to the inputs - Buttons!!

--- Components/profile/Profile.tsx
+++ Components/profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, ScrollView, SafeAreaView, Image } from "react-native";
+import { StyleSheet, View, Text, TextInput, ScrollView, SafeAreaView, Image, TouchableOpacity } from "react-native";
 import CustomButton from "../customButton/CustomButton";
 import { Alert } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../loginModule/LoginModule";
 import ViewContainer from "../viewContainer/ViewContainer";
+import constants from "../../constants";
 
 const Profile = () => {
     const [text, setText] = useState('');
@@ -45,28 +46,101 @@ const Profile = () => {
               <View>
                 <ViewContainer title={'Profile Details'} colour='white' titleColour='white' >
                     <View style = {styles.container}>
-                    <Text style = {styles.labelName}>Full Name</Text>
-                    <TextInput style={styles.input}></TextInput>
-                    <Text>Email Address</Text>
-                    <TextInput style={styles.input}></TextInput>
-                    <Text>Phone Number</Text>
-                    <TextInput style={styles.input} ></TextInput>
+                      <Text style = {styles.labelName}>Full Name</Text>
+                      <View style = {styles.inputWrapper}>
+                        <TextInput 
+                          style={styles.input}
+                          value={text}
+                          onChangeText={setText}
+                          ></TextInput>
+                      </View>
+                    <Text style = {styles.labelName}>Email Address</Text>
+                      <View style = {styles.inputWrapper}>
+                      <TextInput 
+                          style={styles.input}
+                          value={email}
+                          onChangeText={setEmail}
+                          ></TextInput>
+                      </View>
+                    <Text style = {styles.labelName}>Phone Number</Text>
+                     <View style = {styles.inputWrapper}>
+                     <TextInput 
+                          style={styles.input}
+                          value={phoneNumber}
+                          onChangeText={setPhoneNumber}
+                          keyboardType="numeric"
+                          ></TextInput>
+                      </View>
                     </View>
+                </ViewContainer>
+                
+                <ViewContainer title={'Authentication Method'} colour="white" titleColour="white">
+                  <View style={styles.bannerBorder}>
+                    <Icon name="exclamation-circle">
+                      <Text style={styles.bannerLabel}> This client account is using the adhoc authentication method</Text>
+                    </Icon>
+                  </View>
                 </ViewContainer>
 
                 <ViewContainer title={'Password Reset'} colour='white' titleColour='white' >
                     <View style = {styles.container}>
-                    <Text style = {styles.labelName}>Password</Text>
-                    <TextInput style={styles.input}></TextInput>
-                    <Text>New Password</Text>
-                    <TextInput style={styles.input}></TextInput>
-                    <Text></Text>
-                    <TextInput style={styles.input} ></TextInput>
+                    <Text style = {styles.labelName}>Current Password</Text>
+                    <View style = {styles.inputWrapper}>
+                      <TextInput
+                        style={styles.input}
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry={isSecureEntry}
+                      />
+                      <TouchableOpacity onPress={toggleSecureEntry} style={styles.icon}>
+                        <Icon
+                          name={isSecureEntry ? 'eye-slash' : 'eye'}
+                          size={20}
+                          color="gray"
+                        />
+                      </TouchableOpacity>
+                    </View>
+                    <Text style = {styles.labelName}>New Password</Text>
+                      <View style = {styles.inputWrapper}>
+                      <TextInput
+                        style={styles.input}
+                        value={newPassword}
+                        onChangeText={setNewPassword}
+                        secureTextEntry={isSecureEntry}
+                      />
+                      <TouchableOpacity onPress={toggleSecureEntry} style={styles.icon}>
+                        <Icon
+                          name={isSecureEntry ? 'eye-slash' : 'eye'}
+                          size={20}
+                          color="gray"
+                        />
+                      </TouchableOpacity>
+                    </View>
+                    <Text style = {styles.labelName}>New Password Confirmation</Text>
+                    <View style = {styles.inputWrapper}>
+                    <TextInput
+                      style={styles.input}
+                      value={confirmPassword}
+                      onChangeText={setConfirmPassword}
+                      secureTextEntry={isSecureEntry}
+                    />
+                      <TouchableOpacity onPress={toggleSecureEntry} style={styles.icon}>
+                        <Icon
+                          name={isSecureEntry ? 'eye-slash' : 'eye'}
+                          size={20}
+                          color="gray"
+                        />
+                      </TouchableOpacity>
+                    </View>
+                    </View>
+                    <View style={styles.savePasswordButton}>
+                    <CustomButton onPress={() => Alert.alert('Password Changed!')} title="Password Change" color='#42b4ff' iconName='key'></CustomButton>
                     </View>
                 </ViewContainer>
-
-                <CustomButton onPress={() => Alert.alert('Changes Saved')} title="Save Changes" color='#42e83c' iconName='floppy-o'></CustomButton>
-                <CustomButton onPress={() => Alert.alert('Cancel')} title="Cancel" color='#d64f42' iconName='times'></CustomButton> 
+              <View style={styles.buttonBackground}>
+                <CustomButton onPress={() => Alert.alert('Changes Saved')} title="Save Changes" color='#a2fa4b' iconName='floppy-o'></CustomButton>
+                <CustomButton onPress={() => Alert.alert('Cancel')} title="Cancel" color='#faee4b' iconName='times'></CustomButton> 
+              </View>
             </View>
           
           </ScrollView>
@@ -78,43 +152,57 @@ const Profile = () => {
 }
 
 const styles = StyleSheet.create({
-  box:{
-    borderWidth: 1, 
-    borderRadius: 4,
-  },
   input: {
       flex: 1,
       padding: 10,
   },
-  container: {
-    margin: 10,
-  },
-  labelName:{
-
-  },
-  label:{
-    backgroundColor: 'lightblue',
-    fontSize: 16,
-    margin: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    width: 140,
-    textAlign: 'center',
-    borderWidth: 2,
-    borderRadius: 3,
-  },
-  icon: {
-    position: 'absolute', // Position the icon absolutely inside the input container
-    right: 10, // Adjust the right position as needed
-  },
-  inputContainer:{
+  inputWrapper:{
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
     borderColor: 'gray',
     borderRadius: 4,
     paddingHorizontal: 10,
-    marginTop: 5,
+  },
+  container: {
+    margin: 10,
+  },
+  labelName:{
+    fontSize: 14,
+    color: '#1f1f1f',
+    paddingVertical: 10,
+    fontWeight: 'bold',
+  },
+  bannerLabel:{
+    fontSize: 14,
+    color: '#102940',
+    paddingVertical: 10,
+  },
+  bannerBorder:{
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#102940',
+    paddingHorizontal: constants.FONTSIZE.EM/2,
+    marginBottom: constants.FONTSIZE.EM,
+    backgroundColor: '#acedfc',
+  },
+  icon: {
+    position: 'absolute', // Position the icon absolutely inside the input container
+    right: 10, // Adjust the right position as needed
+  },
+  buttonBackground:{
+    backgroundColor: 'white',
+    marginBottom: constants.FONTSIZE.EM,
+    marginTop: constants.FONTSIZE.EM*2,
+    paddingHorizontal: constants.FONTSIZE.EM/2,
+    paddingVertical: constants.FONTSIZE.EM*2,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "grey",
+  },
+  savePasswordButton:{
+    marginBottom: 20,
+    paddingHorizontal: constants.FONTSIZE.EM/3,
   },
 });
 export default Profile;
