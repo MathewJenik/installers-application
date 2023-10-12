98714213337c86856cb93b462c5a0a2136978ccb
T B <tuvshinbaatar1234@gmail.com>
2023-10-12 11:40:19 +1100

    Added in comments

--- components/customAlert/CustomAlert.tsx
+++ components/customAlert/CustomAlert.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import Modal from 'react-native-modal';
 import { TouchableWithoutFeedback, View, Text, StyleSheet } from 'react-native';
-import styling from '../../styling';
 
 interface AlertProps {
   isVisible: boolean;
@@ -10,6 +9,14 @@ interface AlertProps {
   onClose: () => void;
 }
 
+/**
+ * 
+ * @param {boolean} isVisible
+ * @param {string} title
+ * @param {string} message
+ * @param {void} onClose
+ * @returns Modal (Modal alert view)
+ */
 const customAlert: React.FC<AlertProps> = ({ isVisible, title, message, onClose }) => {
   useEffect(() => {
     if (isVisible) {
@@ -27,8 +34,8 @@ const customAlert: React.FC<AlertProps> = ({ isVisible, title, message, onClose 
 
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styling.Styles.Bold_Text}>{title}</Text>
-            <Text style={styling.Styles.Default_Text}>{message}</Text>
+            <Text style={styles.title}>{title}</Text>
+            <Text>{message}</Text>
           </View>
         </View>
         
@@ -60,7 +67,6 @@ const styles = StyleSheet.create({
     elevation: 5
   },
   title: {
-    color: 'black',
     fontWeight: 'bold',
     fontSize: 20,
     marginBottom: 10,
